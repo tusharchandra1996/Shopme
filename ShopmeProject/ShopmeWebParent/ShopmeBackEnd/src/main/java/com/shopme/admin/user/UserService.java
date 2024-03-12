79dc4c0dcbf4a1098fb550f50df9ba3e97cc7220
Tushar Chandra <tusharchandra1996@gmail.com>
2024-03-12 14:47:22 +0530

    Upload project

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -0,0 +1,48 @@
+package com.shopme.admin.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private RoleRepository roleRepo;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public List<User> listAlll() {
+		return (List<User>) userRepo.findAll();
+
+	}
+
+	public List<Role> listroles() {
+		return (List<Role>) roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		ecodePassword(user);
+		userRepo.save(user);
+
+	}
+
+	private void ecodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+	}
+
+	public boolean isEmailUnique(String email) {
+		User userByEmail = userRepo.getUserByEmail(email);
+		return userByEmail == null;
+	}
+}
---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RolerepositoryTests.java
@@ -0,0 +1,44 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class RolerepositoryTests {
+
+	@Autowired
+	private RoleRepository repo;
+
+	@Test
+	public void testCreaateFirstRole() {
+		Role roleAdmin = new Role("Admin", "manage everything");
+		Role saveRole = repo.save(roleAdmin);
+		assertThat(saveRole.getId()).isGreaterThan(0);
+
+	}
+
+	@Test
+	public void testCreaateRestRole() {
+		Role roleSalesPerson = new Role("Salesperson",
+				"manage product price, " + "customer, shipping, orders and sales report");
+		Role roleEditor = new Role("Editor", "manage categories, brands, price, " + "products, articles and menus");
+		Role roleShipper = new Role("Shipper", "view products, view orders " + "and update order status");
+		Role roleAssistant = new Role("Assistant", "manage questions and reviews");
+
+		repo.saveAll(List.of(roleSalesPerson, roleEditor, roleShipper, roleAssistant));
+
+	}
+
+}
